8cb36ac5855ab3a9e6ce43902c3ee18ffbf56f1c
SOM KUMAR <76054385+somkumar9568@users.noreply.github.com>
2024-09-16 23:14:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MainLayout from "./Components/MainLayout"; // Import the new layout
+import MainLayout from "./Components/MainLayout"; 
 import Home from "./Components/Home";
 import Contact from "./Components/Contact";
 import Gallery from "./Components/Gallery";
 import About from "./Components/About";
 import TopSubjects from "./Components/Subject";
 import Course from "./Components/Course";
-import Contactform from "./Components/Contectform"; // Fixed typo in import path
+import Contactform from "./Components/Contectform"; 
 import TeacherCards from "./Components/TeacherCards";
 import TestimonialsSlider from "./Components/TestimonialsSlider";
 import './App.css';
-
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/gallery" element={<Gallery />} />
-          {/* Add more routes as needed */}
+        {/* Define routes with MainLayout */}
+        <Route path="/oksoft" element={<MainLayout />}>
+          {/* Define nested routes */}
+          <Route path="home" element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="gallery" element={<Gallery />} />
         </Route>
-        {/* Route for components that should always be shown */}
-        <Route path="/" element={<MainLayout />}>
+
+        {/* Define route for oksoft page */}
+        <Route path="/oksoft" element={<MainLayout />}>
           <Route
             index
             element={
@@ -48,6 +49,9 @@ function App() {
             }
           />
         </Route>
+
+        {/* Fallback route for non-matching paths */}
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
